79975823675c9de60adb05f73879ee5285b747a7
Sofiya13 <96942363+Sofiya13@users.noreply.github.com>
2023-04-07 17:23:13 +0530

    Update First.java

--- ccss/src/ccss/First.java
+++ ccss/src/ccss/First.java
@@ -4,5 +4,11 @@ public class First {
 
 	public static void main(String[] args) {
 		System.out.println("First class added");
+		
+		int a.b,c;
+		a=18;
+		b=7;
+		c=a+b;
+		System.out.println("The addition is : "+c);
 	}
 }
