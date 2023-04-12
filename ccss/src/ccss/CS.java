25d22972669ced8460f9cafd42ef86445965c3d7
Shaki <Shaki@BEAST>
2023-04-12 21:53:39 +0530

    updated

--- ccss/src/ccss/CS.java
+++ ccss/src/ccss/CS.java
@@ -3,6 +3,11 @@ package ccss;
 public class CS {
   
 	public static void main(String[] args) {
-		System.out.println("Main Code");
+		
+		int a,b;
+		a=10;
+		b=10;
+		System.out.println(a+b);
 	}
 }
+
